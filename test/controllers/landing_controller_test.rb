68680574ce465dd6f8d7df81e73b7e1f83e93aec
Luke Herrington <luke.herrington@appfolio.com>
2019-11-04 16:55:39 -0800

    lh/sp - address coding style issue

--- test/controllers/landing_controller_test.rb
+++ test/controllers/landing_controller_test.rb
@@ -1,10 +1,9 @@
 require 'test_helper'
 
 class LandingControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get root_path
     assert_response :success
     assert_select 'h1', 'Hello World'
   end
-
 end
