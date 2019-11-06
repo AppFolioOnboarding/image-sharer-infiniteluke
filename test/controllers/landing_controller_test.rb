3e1b3f7f995a82e3edc99f9ac13fa116bf9bf0ec
Luke Herrington <luke.herrington@appfolio.com>
2019-11-06 10:35:55 -0800

    lh/sp - remove landing page and related files

--- test/controllers/landing_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LandingControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get root_path
-    assert_response :success
-    assert_select 'h1', 'Hello World'
-  end
-end
