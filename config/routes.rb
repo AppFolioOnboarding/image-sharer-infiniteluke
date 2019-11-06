d9dcc51cb120c494f768e680a5517b7affaf4452
Luke Herrington <luke.herrington@appfolio.com>
2019-11-06 10:35:16 -0800

    lh/sp - add ordered listing view of images as root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'landing#index'
-  resources :images, only: %i[new create show]
+  root 'images#index'
+  resources :images, only: %i[new create show index]
 end
