55dcf99f6e76c001f5756d097bbf81269820675d
Luke Herrington <luke.herrington@appfolio.com>
2019-11-07 11:58:53 -0800

    fix merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root 'images#index'
-  resources :images, only: %i[new create show index destroy]
+  root 'feedbacks#new'
+
+  resources :feedbacks, only: [:new]
 end
