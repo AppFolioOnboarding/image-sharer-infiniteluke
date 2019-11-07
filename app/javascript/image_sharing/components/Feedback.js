37a6b49f51c5a177d779fbdeedef5127577df00b
Luke Herrington <luke.herrington@appfolio.com>
2019-11-07 14:06:55 -0800

    lh/ew - fix style of label/input nesting in Feedback

--- app/javascript/image_sharing/components/Feedback.js
+++ app/javascript/image_sharing/components/Feedback.js
@@ -3,12 +3,12 @@ import React from 'react';
 export default () => (
   <form className="container mx-auto">
     <div className="form-group">
-      <label htmlFor="name">Your Name:
-        <input type="text" className="form-control" id="name" />
+      <label className="w-100" htmlFor="name">Your Name:
+        <input className="form-control"  type="text" id="name" />
       </label>
     </div>
     <div className="form-group">
-      <label htmlFor="comments">Comments:
+      <label className="w-100" htmlFor="comments">Comments:
         <textarea className="form-control" id="comments" />
       </label>
     </div>
